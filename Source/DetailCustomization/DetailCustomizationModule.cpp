822731c2715ed82cbfcfc02690ff40d69d0fd2d8
Shirty <shirtykezrat@gmail.com>
2015-11-07 01:41:51 +0100

    Base requirements met for module compilation (You coud just have a CPP with only IMPLEMENT_MODULE)

---
+++ Source/DetailCustomization/DetailCustomizationModule.cpp
@@ -0,0 +1,8 @@
+#include "EngineMinimal.h"
+
+class FDetailCustomizationModule : public FDefaultGameModuleImpl
+{
+};
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDetailCustomizationModule, DetailCustomization, "DetailCustomization")
